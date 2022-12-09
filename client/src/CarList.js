e39df48feb83c3b55a774ecfcf73644622c8e1b4
Derrick Nyasio <dnyasio2014@gmail.com>
2022-12-09 16:41:30 +0300

    Debugging styled card component on React

--- client/src/CarList.js
+++ client/src/CarList.js
@@ -22,24 +22,7 @@ function CarList() {
 
   return (
     <Wrapper>
-      {cars.length > 0 ? (
-        cars.map((car) => (
-          <Car key={car.id}>
-            <Box>
-              <h2>{car.name}</h2>
-              <p>
-                <img src={car.image_url} alt="car"/>
-                <em>Description: {car.description} minutes</em>
-                &nbsp;·&nbsp;
-                <cite>By {car.user.username}</cite>
-              </p>
-              <Button>
-                Delete Car
-              </Button>
-            </Box>
-          </Car>
-        ))
-      ) : (
+      {cars.length > 0 ? (carCards) : (
         <>
           <h2>No Cars Found</h2>
           <Button as={Link} to="/new">
@@ -56,8 +39,5 @@ const Wrapper = styled.section`
   margin: 40px auto;
 `;
 
-const Car = styled.article`
-  margin-bottom: 24px;
-`;
 
 export default CarList;
