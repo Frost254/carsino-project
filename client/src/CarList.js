05fcd1f423ee3d745fa5b3f018c9597a714912ab
Derrick Nyasio <dnyasio2014@gmail.com>
2022-12-09 16:43:24 +0300

    Fixed dependencies

--- client/src/CarList.js
+++ client/src/CarList.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import CarCard from "./CarCard";
 import styled from "styled-components";
-import { Box, Button } from "./styles";
+import { Button } from "./styles";
 
 function CarList() {
   const [cars, setCars] = useState([]);
